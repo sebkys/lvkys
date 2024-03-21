ecc3bba3614a236c085c78c61f108de480f8eadb
satanismonchain <45162788+sebkys@users.noreply.github.com>
2024-03-21 21:10:22 +0700

    Update run.sh
    changed rpc

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-b5029f8d-b1b0-49eb-96a9-a479b3b09b5d/)
 
 
 echo "$curl_output" >> Update.tsx
